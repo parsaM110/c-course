c8d431ca8fd701ba7519591e80b77c2f7caaa632
parsam110 <pmomeni80@gmail.com>
2025-02-25 18:16:19 +0330

    lib files and string

--- 04.c
+++ 04.c
@@ -1,26 +1,10 @@
+#include <stdio.h>
+
 int main(){
 
-    struct AddressIPV4{
-        char ip[16]; // 4 * 3 + 3 =>  15 => 16 (last char should be null for str)
-        int port ;
-    };
 
-    struct AddressIPV6{
-        char ip[42]; // 32 + 7 => 41 => 42
-         int port ;
-    };
-
-    Union Address{
-        struct AddressIPV4 v4;
-        struct AddressIPV6 v6;
-    };
-
-    union Address A;
-    A.v4.ip = "192.168.12.13";
-    A.v4.port = 80;
-
-   
-
-
-
+    // char b[] = "secret";
+    char *b = "secret";
+    printf("%s\n", b);
+    return 0;
 }
