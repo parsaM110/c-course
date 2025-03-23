c5701b46e37b6bbf36b2cbb3413224a7f3148193
parsam110 <pmomeni80@gmail.com>
2025-03-23 22:56:55 +0330

    05: define helper.c

---
+++ random_data/helper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void generate_random_numbers(int *buf, size_t count, int min, int max){
+    
+    srandom(time(NULL));
+    for(int i=0; i < count; i++){
+        *(buf+i) = min + random() % (max - min+1);
+    }
+
+}
+
+void print_array(const int *buf, size_t count){
+
+    for(int i=0; i < count; i++){
+        printf("%d ", *(buf + i));
+    }
+
+    printf("\n");
+
+}
--- random_data/main.c
+++ random_data/main.c
@@ -1,45 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> // sizeof is from stdio
 
-// it is hard to use this damn sizeof
 # define SIZE(A) sizeof(A)/ sizeof(A[0])
 
 void generate_random_numbers(int *buf, size_t count, int min, int max);
 void print_array(const int *buf, size_t count);
 
+// you can omit declaration but if the implementation is wrong gcc would not figure it out
+// older gcc will throw error and not compile
+// clang will throw error too
 
 int main(){
 
     int buf[20];
-    // generate_random_numbers(buf, sizeof(buf)/sizeof(buf[0]), 10, 50);
-    // print_array(buf, sizeof(buf)/sizeof(buf[0]));
 
     generate_random_numbers(buf, SIZE(buf), 10, 50);
     print_array(buf, SIZE(buf));
-
-    //remember random number for each 1 second
 
     return 0;
 
 
 }
 
-void generate_random_numbers(int *buf, size_t count, int min, int max){
-    
-    srandom(time(NULL));
-    for(int i=0; i < count; i++){
-        *(buf+i) = min + random() % (max - min+1);
-    }
-
-}
-
-void print_array(const int *buf, size_t count){
-
-    for(int i=0; i < count; i++){
-        printf("%d ", *(buf + i));
-    }
-
-    printf("\n");
-
-}
