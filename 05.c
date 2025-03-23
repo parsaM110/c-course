ca5fff4dbe7156ea91ce4e9e19014df6b8666c2b
parsam110 <pmomeni80@gmail.com>
2025-03-23 21:31:22 +0330

    05: talking aobut c standards in fuction

--- 05.c
+++ 05.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void print_array(int[],size_t);
-
+void print_array2(const int[],size_t);
 
 int main(){
 
     int A[11] = {1,2,3,4,5,6,7,8,9,10,11};
-    print_array(A,20000);
+    print_array2(A,3);
     // print_array(A,sizeof(A)/sizeof(A[0]));
 
 
@@ -14,13 +14,19 @@ int main(){
 }
 
 void print_array(int ARR[], size_t nmemb){ 
-    // nmemb : number of members (max: is pointer 64 byte)
-    // but using long long unsigned int nmemb is only on my system
-    // instead we use size_t
- 
+    
 
-    for(int i=0; i< nmemb; i++){ // the 10 is static , well we can put it as param
-        printf("%d\n", ARR[i]); // Magic Value -> 10 or use MACRO or constant (no hard code)
+    for(int i=0; i< nmemb; i++){ 
+        printf("%d\n", ARR[i]); 
 
+    }
+}
+
+void print_array2(const int *ptr, size_t nmemb){ 
+
+    for(int i=0; i< nmemb; i++){ 
+        // printf("%d\n", ptr[i]); // ptr[i] in c is like dereference and pointer arithmetic  == *(ptr+i)
+        // printf("%d\n", *(ptr+i));
+        printf("%d\n", *(ptr++));
     }
 }
