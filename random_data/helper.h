5c8dbaf5470a40471269aade813c1ddf9b05ae6a
parsam110 <pmomeni80@gmail.com>
2025-03-23 23:07:12 +0330

    05: define header gaurd

--- random_data/helper.h
+++ random_data/helper.h
@@ -1,4 +1,10 @@
+// Header Guard MACRO for avoiding redeclaration
+#ifndef HELPER_H
+#define HELPER_H
+
 #include <stdlib.h> // becuase I use size_t
 
 void generate_random_numbers(int *buf, size_t count, int min, int max);
 void print_array(const int *buf, size_t count);
+
+#endif
