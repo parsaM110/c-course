847bbb2e3eb7cb34e14e93fecd96fb4d0c8ab298
parsam110 <pmomeni80@gmail.com>
2025-03-28 01:27:14 +0330

    08: implement the whole thing by functions| not tested yet �

--- 07.c
+++ 07.c
@@ -5,53 +5,69 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
+int get_file_info(const char *path, struct stat *info){
+
+    struct stat info;
+    if(stat(path, &info) == -1){
+        // fprintf(stderr, "Error geting file info\n");
+        // exit(1); //never use exit in function -> because if you are in middle of something it aborts
+        // function should only except error
+        // also it is better not to print in function
+        return -1; 
+    }
+
+    return 0;
+
+}
+
+int read_file(const char *path, char *content, size_t blocksize){
+    
+    FILE *f;
+    
+
+    size_t file_size = fileinfo.st_size;
+
+    char buffer[fileinfo.st_blksize];  // you could use statvfs too , it also applies to folders
+    size_t size;
+    size_t total=0;
+
+     if( (f = fopen(name,"r")) == NULL){
+        return -1;
+        //fprintf(stderr, "Failed to open file\n");
+        //exit(1);
+    }
+
+    do{
+        size = fread(buffer, sizeof(char), sizeof(buffer)/sizeof(char), f);
+        memcpy(content+total, buffer, size);
+        total += size;
+
+    } while(size == 4096);
+      fclose(f); 
+      return 0;
+
+}
+
 int main(){
 
-
-    FILE *f;
+    
     char *name = "data.txt";
-    struct stat fileinfo;
 
-    if(stat(name, &fileinfo) == -1){
+    struct stat info;
+    if(get_file_info(name, &info)){
         fprintf(stderr, "Error geting file info\n");
         exit(1);
     }
 
-    size_t file_size = fileinfo.st_size;
-    char *file_content = malloc(file_size);
-
-    if( (f = fopen(name,"r")) == NULL){
-        printf(stderr, "Failed to open file\n");
-        exit(1);
+    char *file_content = malloc(info.st_size);
+    if(read_file(name, file_content, info_st_blksize)){
+        fprintf(stderr, "Failed to open file\n");
+        exit(2);
     }
 
-    char buffer[4096]; //block size (hard use block not byte for measure)
-
-    //how to prevent stack overflow ?
-    // it is in stack if you read a lot in stack, this can happen
-    // in heap also makes performance issues
-    // so we also make a fixed size buffer 
-    // and in some cases if you have additional 
-    size_t size;
-    size_t total=0;
-
-    do{
-        size = fread(buffer, sizeof(char), sizeof(buffer)/sizeof(char), f);
-        memcpy(file_content+total, buffer, size);
-        total += size;
-
-    } while(size == 4096);
-
-
-    printf("%ld bytes read from file\n", size);
-
-
-    
-
-    
-    
-    fclose(f); 
     free(file_content);
+    // if you exit then this free won't work, now it is okay because by exit here we free memory
 
     return 0;
 }
