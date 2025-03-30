f5edb17f6e166c1d684b191201dc8caeb61ed678
parsam110 <pmomeni80@gmail.com>
2025-03-31 01:43:16 +0330

    13 method 2 for empty the buffer

--- 13.c
+++ 13.c
@@ -10,19 +10,16 @@ int main(){
     int age;
      while(1){
         printf("Please enter your Age: ");
-        // there are lot of ways to empty buffer
-        // you can put all buffers to garbage
-        // this is one of the ways
         if(scanf("%d", &age) == 1)
             break;
-        else    
-            scanf("%*[^\n]"); 
-        // read until new line 
-        // if you were not successful empty the buffer
-        // you can also use gets
+        else 
+            //fseek(stdin, 0, SEEK_END);  
+            //fflush(stdin)
 
-        // you can use fflush
-        // flush the buffer but for input stream it has not stable behaviour
+            // 👆 these wont work because of issues with stdin
+            while(getchar()!='\n');
+
+           // scanf("%*[^\n]"); 
 
     }
     return 0;
