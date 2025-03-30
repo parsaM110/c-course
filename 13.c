937f87da0a98916f913097d9b23596cfd8a57154
parsam110 <pmomeni80@gmail.com>
2025-03-31 01:40:18 +0330

    13 method 1 for empty the buffer

--- 13.c
+++ 13.c
@@ -6,28 +6,24 @@
 
 
 int main(){
-    // char buf[255];
-
-    // while(1){
-    //     printf("Please enter your name: ");
-    //     // scanf("%s", buf);
-    //     // scanf returns numbers of items it received 
-    //     if(scanf("%s", buf) == 1)
-    //         break;
-    // }
 
     int age;
      while(1){
         printf("Please enter your Age: ");
-
+        // there are lot of ways to empty buffer
+        // you can put all buffers to garbage
+        // this is one of the ways
         if(scanf("%d", &age) == 1)
             break;
+        else    
+            scanf("%*[^\n]"); 
+        // read until new line 
+        // if you were not successful empty the buffer
+        // you can also use gets
 
-        // if you give invalid number like test
-        // it stays in scanf buffer
-        // scanf doesn't consume it 
-        // cause it always be fail
-        // infinite printf loop
+        // you can use fflush
+        // flush the buffer but for input stream it has not stable behaviour
+
     }
     return 0;
 }
