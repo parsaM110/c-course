7cbf521018f9bb7cba3d798ba8eb0a9f9b750340
parsam110 <pmomeni80@gmail.com>
2025-03-31 00:52:44 +0330

    13 running the background service by fork

--- 13.c
+++ 13.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-void worker(){
-    if(fork()!=0) return; // I want this only be for child and parent doesn't do this
-    // if you uncomment this parent will see this too
-    printf("OK I am done (Child process - %d)\n", getpid());
 
-    // ...
-    exit(0); // we want the code after worker() be specific for perent; 
-    //if you comment it then child will do the code after worker() too
-}
 int main(){
-    worker();
-    printf("ok I am done (main process - %d)\n", getpid());
+    printf("Ok, going to background ...\n");
+
+    sleep(5);
+
+    // if(fork()!=0) exit(0); //parent is exited 
+    if(fork()!=0) {
+        sleep(10);
+        exit(0);
+    }
+
+
+
+    sleep(60); // this is child
     return 0;
 }
