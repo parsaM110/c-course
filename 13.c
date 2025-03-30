bb024742d98cf1a67b63ae03ac094546416e2f40
parsam110 <pmomeni80@gmail.com>
2025-03-31 01:49:03 +0330

    13 disable SIGINT, i dont know why now if you press ctrl+D it goes to loop again

--- 13.c
+++ 13.c
@@ -2,24 +2,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <signal.h>
 
-
+void handler(int sig){
+    printf("\n\n\n SIGNAL %d HAPPEND\n", sig);
+}
 
 int main(){
-
     int age;
+    signal(SIGINT, handler);
+
+
      while(1){
         printf("Please enter your Age: ");
         if(scanf("%d", &age) == 1)
             break;
         else 
-            //fseek(stdin, 0, SEEK_END);  
-            //fflush(stdin)
-
-            // 👆 these wont work because of issues with stdin
-            while(getchar()!='\n');
-
-           // scanf("%*[^\n]"); 
+            scanf("%*[^\n]"); 
 
     }
     return 0;
