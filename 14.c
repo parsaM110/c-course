c8cbb8f0eb5c4f1507779ef356adc0bbfb1a36d9
parsam110 <pmomeni80@gmail.com>
2025-04-01 20:58:47 +0330

    solving the bug by gdb

--- 14.c
+++ 14.c
@@ -4,25 +4,13 @@
 #include <string.h>
 
 
-void test(){
-    printf("Hello world\n");
-}
-
-void test2(int a){
-    printf("Hello world %d!\n",a);
-}
-
-size_t copy_and_filter(const int *data, size_t count, size_t size,void *out,
-                         int (*f)(const void *)){
+size_t copy_and_filter(const void *data, size_t count, size_t size,
+                        void *out,int (*f)(const void *)){
 
     int out_counter = 0;
-    for(int i = 0;i<count;i++){
-        // if(f(data+i*size) != 0){    // void * -> no Pointer arithmetics
-            // but in GCC void* + 1 works
-         if(f(  (void *) ( (char *) data+i*size) )!= 0){  // this now is standard in C
-         // casting to *char common is very popular for making it to pointer of bytes
-         // and by this you can access to single bytes possible
-            memcpy(out+out_counter*size, data+i*size, size);
+    for(int i = 0; i < count ;i++){
+         if(f(  (void *) ( (char *) data+i*size) )!= 0){  
+            memcpy(out+(out_counter++)*size, data+i*size, size);
         }
     }
     return out_counter;
