ced3a397d7ab5fd6fc17d6c15d134f1ba121623c
parsam110 <pmomeni80@gmail.com>
2025-04-01 17:53:25 +0330

    14 use MACRO for Swap sort

--- 14.c
+++ 14.c
@@ -9,7 +9,7 @@
                                 printf("%d ",A[i]);\
                             printf("\n");\
                         }while (0);
-
+#define SWAP(A,B) do{A =A ^ B; B = B ^ A; A= A ^ B;}while(0);
 
 static inline void generate_randoms(int *buf, size_t count){
         srandom(time(NULL));
@@ -22,10 +22,7 @@ void sort(int *buf, size_t count){
     for(int i=0; i<count; i++)
         for(int j=0; j<count-1; j++)
             if(buf[j] > buf[j+1]){
-                //xor swap -> for integer
-                buf[j] = buf[j] ^ buf[j+1];
-                buf[j+1] = buf[j+1] ^ buf[j];
-                buf[j] = buf[j] ^ buf[j+1];
+                SWAP(buf[j], buf[j+1]);
             }
 }
 
