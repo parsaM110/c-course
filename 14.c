e3731b5cecb6f247b45e22e2142228d0052f6765
parsam110 <pmomeni80@gmail.com>
2025-04-01 19:10:02 +0330

    implement and use copy_and_filter| which returns even numbers

--- 14.c
+++ 14.c
@@ -11,35 +11,31 @@ void test2(int a){
     printf("Hello world %d!\n",a);
 }
 
-int *copy_and_filter(const int *data, size_t count, int **out){
+size_t copy_and_filter(const int *data, size_t count, int *out){
     // if you malloc you can allocate with exact size
     // but you cant get data from stack and you should use heap
     // I will malloc my self and will return the memory address 
     // I will return count and for error handling cant be used because size_t is unisigend int
 
-    int out_counter =0;
+    int out_counter = 0;
     for(int i = 0;i<count;i++){
         if(data[i] % 2 == 0){
             out[out_counter++] = data[i];
-        return out_counter;
         }
     }
+    return out_counter;
 }
 
 int main(){
-    //simple func 
-    // test();
+    int A[10] = {5,6,7,8,9,10,11,12,13,14};
+    int B[10];
 
+    size_t count = copy_and_filter(A, 10, B);
+    for(int i=0; i<count; i++){
+        printf("%d ", B[i]);
+    }
+    printf("\n");
 
-    // void() *
-    void (*func)(int); //func is pointer function name
-
-    func = test2;
-    // or (both works)
-    func = &test2;
-
-
-    func(20); //is like test(20)
     return 0;
 
 }
