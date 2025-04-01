b9e98489faf72e13124f37d2250a17ff38e2f501
parsam110 <pmomeni80@gmail.com>
2025-04-01 16:23:29 +0330

    14 using inline function

--- 14.c
+++ 14.c
@@ -3,23 +3,26 @@
 
 
 #define SIZE(A) (sizeof(A)/sizeof(A[0]))
-#define PRINT_ARRAY(A)  do{\
-                            for(int i=0; i<SIZE(A); i++)\
+#define PRINT_ARRAY(A,C)  do{\
+                            for(int i=0; i<C; i++)\
                                 printf("%d ",A[i]);\
                             printf("\n");\
                         }while (0);
 
+
+static inline void generate_randoms(int *buf, size_t count){
+        srandom(10);
+    for(int i=0; i < 10; i++)
+        buf[i] = random();
+}
+
 int main(){
     int n[10];
-
-    srandom(10);
-    for(int i=0; i < 10; i++)
-        n[i] = random();
-
-    
-    PRINT_ARRAY(n);
+    generate_randoms(n, SIZE(n));
+    PRINT_ARRAY(n, SIZE(n));
 
 }
 
 //problem with this code is in MACRO the method I use for print is for stack array
-// for pointer arrays(allocated by malloc) it won't work -> segmentation fault
+// for pointer arrays(allocated by malloc) it won't work (can't get the size because it is dynamic) -> segmentation fault
+// it is better to get size manually and not documentation(no body read that)
