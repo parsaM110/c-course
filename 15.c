e87ef09eea6bf5b21dc06f5717f019dd9855415e
parsam110 <pmomeni80@gmail.com>
2025-04-02 03:05:28 +0330

    15 permanent priviliage drop

--- 15.c
+++ 15.c
@@ -7,7 +7,22 @@ int main(){
 
 printf("UID: %d\n", getuid());
 printf("EUID: %d\n", geteuid());
+printf("__________________\n");
+// Temporary privilege drop
 seteuid(33);
+printf("UID: %d\n", getuid());
+printf("EUID: %d\n", geteuid());
+printf("__________________\n");
+seteuid(0);
+printf("UID: %d\n", getuid());
+printf("EUID: %d\n", geteuid());
+printf("__________________\n");
+// Permanent  privilege drop
+setuid(33);
+printf("UID: %d\n", getuid());
+printf("EUID: %d\n", geteuid());
+printf("__________________\n");
+setuid(0);
 printf("UID: %d\n", getuid());
 printf("EUID: %d\n", geteuid());
 return 0;
