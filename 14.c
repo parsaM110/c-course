ec6e3349d2c499c2be68b1f061d3d3b06de35096
parsam110 <pmomeni80@gmail.com>
2025-04-01 21:20:32 +0330

    implement qsort

--- 14.c
+++ 14.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 
-size_t copy_and_filter(const void *data, size_t count, size_t size,
-                        void *out,int (*f)(const void *)){
+#define SIZE(A) (sizeof(A)/sizeof(A[0]))
+#define PRINT_ARRAY(A,C)  do{\
+                            for(int i=0; i<C; i++)\
+                                printf("%d ",A[i]);\
+                            printf("\n");\
+                        }while (0);
+#define SWAP(A,B) do{A =A ^ B; B = B ^ A; A= A ^ B;}while(0);
 
-    int out_counter = 0;
-    for(int i = 0; i < count ;i++){
-         if(f(  (void *) ( (char *) data+i*size) )!= 0){  
-            memcpy(out+(out_counter++)*size, data+i*size, size);
-        }
-    }
-    return out_counter;
+static inline void generate_randoms(int *buf, size_t count){
+        srandom(time(NULL));
+    for(int i=0; i < 10; i++)
+        buf[i] = random() % 90 + 10;
 }
 
-int is_even(const void *x){
-    int v = *((int *)x);
-    return v % 2 == 0;
+int is_greater(const void *p1, const void *p2){
+    int a = *((int*) p1);
+    int b = *((int *) p2);
+    return a - b;  // you can do a > b also
+
+}
+
+
+// q sort
+void sort(int *buf, size_t count){
+    // for(int i=0; i<count; i++)
+    //     for(int j=0; j<count-1; j++)
+    //         if(buf[j] > buf[j+1]){
+    //             SWAP(buf[j], buf[j+1]);
+    //         }
+
+    qsort(buf, count, sizeof(int), is_greater); 
 }
 
 int main(){
-    int A[10] = {5,6,7,8,9,10,11,12,13,14};
-    int B[10];
-
-    size_t count = copy_and_filter((const void *)A, 10, sizeof(int), (void *)B, is_even);
-    for(int i=0; i<count; i++){
-        printf("%d ", B[i]);
-    }
-    printf("\n");
-
-    return 0;
+    int n[10];
+    generate_randoms(n, SIZE(n));
+    PRINT_ARRAY(n, SIZE(n));
+    sort(n, SIZE(n));
+    PRINT_ARRAY(n, SIZE(n));
 
 }
