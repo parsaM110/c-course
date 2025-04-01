e4286c0bec5b7a366ad7af2fd6e5f5eda2c4d899
parsam110 <pmomeni80@gmail.com>
2025-04-01 23:15:42 +0330

    14 creat thread but main process is longer than thread

--- 14.c
+++ 14.c
@@ -1,48 +1,26 @@
 #include <stdio.h>
-#include <bsd/stdlib.h>
-#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 
+void *my_thread(void *data){
 
-#define SIZE(A) (sizeof(A)/sizeof(A[0]))
-#define PRINT_ARRAY(A,C)  do{\
-                            for(int i=0; i<C; i++)\
-                                printf("%d ",A[i]);\
-                            printf("\n");\
-                        }while (0);
-#define SWAP(A,B) do{A =A ^ B; B = B ^ A; A= A ^ B;}while(0);
-
-static inline void generate_randoms(int *buf, size_t count){
-        srandom(time(NULL));
-    for(int i=0; i < 10; i++)
-        buf[i] = random() % 90 + 10;
+    sleep(3);
+    printf("Hello Thread\n");
+    return NULL; //if you don't want to return anything return NULL
 }
 
-int is_greater(const void *p1, const void *p2){
-    int a = *((int*) p1);
-    int b = *((int *) p2);
-    return a - b;  // you can do a > b also
-
-}
-
-
-// q sort
-void sort(int *buf, size_t count){
-    // for(int i=0; i<count; i++)
-    //     for(int j=0; j<count-1; j++)
-    //         if(buf[j] > buf[j+1]){
-    //             SWAP(buf[j], buf[j+1]);
-    //         }
-
-    //qsort(buf, count, sizeof(int), is_greater); 
-    mergesort(buf, count, sizeof(int), is_greater); 
-}
 
 int main(){
-    int n[10];
-    generate_randoms(n, SIZE(n));
-    PRINT_ARRAY(n, SIZE(n));
-    sort(n, SIZE(n));
-    PRINT_ARRAY(n, SIZE(n));
+
+    pthread_t t1;
+    // threads get void* and return void*
+
+    pthread_create(&t1,NULL, my_thread, NULL);
+
+    sleep(5);
+    printf("main finished\n");
+    return 0;
 
 }
