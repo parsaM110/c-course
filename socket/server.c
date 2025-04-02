1a2676fc626daba4d7f58a69417634a8d0aa6d63
parsam110 <pmomeni80@gmail.com>
2025-04-02 04:19:37 +0330

    succesfully implement server

--- socket/server.c
+++ socket/server.c
@@ -10,7 +10,7 @@
 
 int main(){
 
-    int server_fd, client_socketrt_fd, opt=1;
+    int server_fd, client_fd, opt=1;
     struct sockaddr_in address;  
     if( (server_fd = socket(AF_INET, SOCK_STREAM, 0 )) < 0){
         perror("Error creating socket"); 
@@ -22,7 +22,7 @@ int main(){
     }
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_family = AF_INET;
-    address.sin_port = htons(80); //Byte order convert
+    address.sin_port = htons(9876); //Byte order convert
 
 
     if( bind(server_fd,(const struct sockaddr *)&address, sizeof(address)) == -1){
@@ -30,6 +30,26 @@ int main(){
         exit(1);
     }
 
+    if(listen(server_fd, 10) == -1){ //for 10 req we have backlog 
+        perror("Cannot listen");
+        exit(1);
+    }
+
+    struct sockaddr_in client_addr;
+    int clientsize = sizeof(client_addr);
+
+    while(1){
+        printf("wainting for connection ...\n");
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&clientsize);
+        printf("New Connection received\n");
+        char buf[1024];
+        size_t len;
+
+        len = read(client_fd, buf, 1024);
+        send(client_fd, "Hello!", 6,0);
+        send(client_fd, buf, len, 0);
+        // in tcp we don't have additional feature in layer 4
+    }
 
     return 0;
     
